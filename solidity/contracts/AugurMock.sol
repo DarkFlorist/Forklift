2ef634a4242f658c9b37b917134d2f888320438d
KillariDev <killari@gmail.com>
2025-03-17 15:10:53 +0200

    get contracts to compile + update solidity - remove safe math

--- solidity/contracts/AugurMock.sol
+++ solidity/contracts/AugurMock.sol
@@ -1,10 +1,9 @@
-pragma solidity 0.6.10;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { IERC20 } from "./IERC20.sol";
 
 contract ShareToken {
-	using SafeMath for uint256;
 
 	IERC20 public dai;
 	mapping (address => mapping(uint256 => uint256)) public balanceOf;
@@ -18,7 +17,7 @@ contract ShareToken {
 	event TransferBatch(address indexed operator, address indexed from, address indexed to, uint256[] ids, uint256[] values);
 	event ApprovalForAll(address indexed account, address indexed operator, bool approved);
 
-	constructor(IERC20 _dai) public {
+	constructor(IERC20 _dai) {
 		dai = _dai;
 	}
 
@@ -31,8 +30,8 @@ contract ShareToken {
 		require(to != address(0), "ERC1155: transfer to the zero address");
 		require(from == msg.sender || isApprovedForAll[from][msg.sender], "ERC1155: caller is not owner nor approved");
 
-		balanceOf[from][id] = balanceOf[from][id].sub(amount, "ERC1155: insufficient balance for transfer");
-		balanceOf[to][id] = balanceOf[to][id].add(amount);
+		balanceOf[from][id] = balanceOf[from][id] - amount;
+		balanceOf[to][id] = balanceOf[to][id] + amount;
 
 		emit TransferSingle(msg.sender, from, to, id, amount);
 	}
@@ -44,31 +43,31 @@ contract ShareToken {
 		for (uint256 i = 0; i < tokenIds.length; ++i) {
 			uint256 tokenId = tokenIds[i];
 			uint256 value = values[i];
-			balanceOf[from][tokenId] = balanceOf[from][tokenId].sub(value);
-			balanceOf[to][tokenId] = balanceOf[to][tokenId].add(value);
+			balanceOf[from][tokenId] = balanceOf[from][tokenId] - value;
+			balanceOf[to][tokenId] = balanceOf[to][tokenId] + value;
 			emit TransferBatch(msg.sender, from, to, tokenIds, values);
 		}
 	}
 
 	function publicBuyCompleteSets(address market, uint256 amount) external returns (bool) {
-		uint256 cost = amount.mul(numTicks);
+		uint256 cost = amount * numTicks;
 
 		dai.transferFrom(msg.sender, address(this), cost);
 
 		for (uint256 i = 0; i <= numOutcomes; ++i) {
 			uint256 tokenId = getTokenId(market, i);
-			balanceOf[msg.sender][tokenId] = balanceOf[msg.sender][tokenId].add(amount);
+			balanceOf[msg.sender][tokenId] = balanceOf[msg.sender][tokenId] + amount;
 		}
 
 		return true;
 	}
 
 	function publicSellCompleteSets(address market, uint256 amount) external returns (bool) {
-		uint256 payment = amount.mul(numTicks).mul(feeNumerator).div(feeDenominator);
+		uint256 payment = amount * numTicks * feeNumerator / feeDenominator;
 
 		for (uint256 i = 0; i <= numOutcomes; ++i) {
 			uint256 tokenId = getTokenId(market, i);
-			balanceOf[msg.sender][tokenId] = balanceOf[msg.sender][tokenId].sub(amount);
+			balanceOf[msg.sender][tokenId] = balanceOf[msg.sender][tokenId] - amount;
 		}
 
 		dai.transfer(msg.sender, payment);
--- solidity/contracts/DaiMock.sol
+++ solidity/contracts/DaiMock.sol
@@ -1,9 +1,10 @@
-pragma solidity 0.6.10;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "./ERC20.sol";
 
 contract Dai is ERC20 {
-	constructor() ERC20("DAI", "DAI") public { }
+	constructor() ERC20("DAI", "DAI") { }
 
 	function mint(uint256 amount) external {
 		_mint(msg.sender, amount);
