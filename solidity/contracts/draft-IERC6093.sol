8aee7db77d8e45bcfe649feec9ec6b2eb4fcd5d0
KillariDev <killari@gmail.com>
2025-03-18 10:16:09 +0200

    update solidity project

--- solidity/contracts/draft-IERC6093.sol
+++ solidity/contracts/draft-IERC6093.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.1.0) (interfaces/draft-IERC6093.sol)
-pragma solidity 0.8.26;
+pragma solidity 0.8.29;
 
 /**
  * @dev Standard ERC-20 Errors
