6b1b9df4a6735b032b238f24235fd24cfcb2c9a4
KillariDev <killari@gmail.com>
2025-04-16 13:43:08 +0300

    change safe seconds

--- app/ts/utils/utils.ts
+++ app/ts/utils/utils.ts
@@ -1,5 +1,5 @@
 export const bigintSecondsToDate = (seconds: bigint) => {
-	if (seconds > Number.MAX_SAFE_INTEGER) throw new Error(`Too big seconds value: ${ seconds }`)
+	if (seconds > 8640000000000n) throw new Error(`Too big seconds value: ${ seconds }`)
 	if (seconds < 0) throw new Error(`Got negative seconds: ${ seconds }`)
 	return new Date(Number(seconds) * 1000)
 }
