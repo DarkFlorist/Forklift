2ed985b28717f6f9b31352ddde23c2aa2ad020e2
KillariDev <killari@gmail.com>
2025-04-23 11:22:38 +0300

    seems like `_scalarDenomination` can also be `false`, map that to undefined

--- app/ts/CreateMarketUI/types/createMarketTypes.ts
+++ app/ts/CreateMarketUI/types/createMarketTypes.ts
@@ -1,4 +1,5 @@
 import * as funtypes from 'funtypes'
+import { LiteralConverterParserFactory } from '../../types/types.js'
 
 export type ExtraInfo = funtypes.Static<typeof ExtraInfo>
 export const ExtraInfo = funtypes.Intersect(
@@ -10,6 +11,6 @@ export const ExtraInfo = funtypes.Intersect(
 		tags: funtypes.ReadonlyArray(funtypes.String),
 		longDescription: funtypes.String,
 		template: funtypes.Unknown,
-		_scalarDenomination: funtypes.String,
+		_scalarDenomination: funtypes.Union(funtypes.String, funtypes.Literal(false)).withParser(LiteralConverterParserFactory<false | string, undefined>(false, undefined))
 	})
 )
